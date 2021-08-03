b25429fb26e12f48f18cfdaa107ad2164e8d7aea
新城旧梦 <39694377+dadafox@users.noreply.github.com>
2021-08-03 10:46:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: '站点状态监控---极客青年',
+  SiteName: '站点状态监控',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,7 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两种 Api Key
   ApiKeys: [
-    'ur894242-90d1eb90acc82624bcaed27a'
+    'ur906596-006152717ceacd6cfe09a91a'
   ],
 
   // 是否显示监测站点的链接
@@ -19,21 +19,13 @@ window.Config = {
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
   // 不过时间不要设置太长，容易卡，接口请求也容易失败
-  CountDays: 60,
+  CountDays: 7,
 
   // 导航栏菜单
   Navi: [
     {
-      text: '项目主页',
-      url: 'https://github.com/geekyouth/uptime-status'
-    },
-    {
-      text: 'GitHub',
-      url: 'https://github.com/geekyouth/'
-    },
-    {
-      text: '博客',
-      url: 'https://java666.cn'
+      text: 'Status',
+      url: 'https://status.itxcjm.top'
     }
   ]
 };
